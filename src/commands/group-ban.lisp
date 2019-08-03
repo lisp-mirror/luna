a84680622d3ec9df5bc20a99498adcfe7c876a6d
Gnuxie <gnuxie@protonmail.com>
2019-08-03 22:51:13 +0100

    ban
    no test yet...

---
+++ src/commands/group-ban.lisp
@@ -0,0 +1,28 @@
+#| This file is part of luna
+   Copyright (C) 2019 Gnuxie <Gnuxie@protonmail.com>|#
+
+(in-package #:luna)
+
+(define-step room-ban (target target-user reason)
+  (unless (has-power-p target (cl-matrix:username cl-matrix:*account*) "ban")
+    (error 'luna-permission-error :description
+           (format nil "~a doesn't have permission in ~a to ban users." (cl-matrix:username cl-matrix:*account*) target)))
+
+  (cl-matrix:room-ban target-user reason target))
+
+(define-step ban (control group sender target-user reason &rest targets)
+  (unless (has-power-p control sender "ban")
+    (error 'luna-permission-error :description
+           (format nil "~a doesn't have permission to ban in this room.")))
+
+  (remove-if-not (lambda (f) (typep f 'error))
+   (mapgroup (luna-lambda (r)
+                (room-ban r target-user reason))
+              control group)))
+
+(define-command-parser ban (name rest room-id event)
+  (declare (ignore name))
+  (cl-ppcre:register-groups-bind (group-name target-user reason)
+      ("^(\\S+)\\s+(\\S+)\\s+(.+)?" rest)
+    (when (and target-user reason)
+      (apply #'ban room-id group-name (jsown:val event "sender") targets))))
