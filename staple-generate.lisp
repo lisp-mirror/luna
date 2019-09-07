cab1e146bff801f48524a1d0ea3ebc9ffb6ddaa3
Gnuxie <gnuxie@protonmail.com>
2019-09-07 22:53:26 +0100

    generate doc

--- staple-generate.lisp
+++ staple-generate.lisp
@@ -3,13 +3,12 @@
 (ql:quickload :luna)
 (ql:quickload :staple-markdown)
 (ql:quickload :staple-markless)
-(load (asdf:system-relative-pathname :luna "scripts/start-bot-args.lisp"))
 
 (loop :while (null (find-package "STAPLE")) :do
      (sleep 1))
 
 (staple:generate :luna
-                 :subsystems (list (list (asdf:find-system "luna.framework") :packages (list :luna.framework :luna.framework.hooks :luna.framework.config)))
+                 :subsystems (list (list (asdf:find-system "luna.framework") :packages (list :luna.framework :luna.framework.hooks)))
                  :packages (list :luna :luna.hooks)
                  :if-exists :supersede
                  :images (list (asdf:system-relative-pathname :luna "doc/luna-red.svg"))
